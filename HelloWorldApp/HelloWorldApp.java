4ce9628a83fa323e7457a27b565fa428a58fc224
Fabio Schunig <fabio.schunig@gmail.com>
2018-08-27 17:18:12 -0300

    Hello World Application

---
+++ HelloWorldApp/HelloWorldApp.java
@@ -0,0 +1,13 @@
+/*
+https://docs.oracle.com/javase/tutorial/getStarted/cupojava/win32.html
+*/
+
+/**
+ * The HelloWorldApp class implements an application that
+ * simply prints "Hello World!" to standard output.
+ */
+class HelloWorldApp {
+    public static void main(String[] args) {
+        System.out.println("Hello World!"); // Display the string.
+    }
+}
